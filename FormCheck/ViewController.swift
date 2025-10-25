c7009988b1e16a3196df10555f5caa6f7fcedfec
Khoi Pham <pkhoi1309@gmail.com>
2025-10-25 17:39:40 -0400

    Initial Commit

---
+++ FormCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FormCheck
+//
+//  Created by Khoi on 25/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
